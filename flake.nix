b6717f64a9c378fcd9f2c88de565468d5c036bc8
Nick Cao <nickcao@nichi.co>
2022-09-17 14:13:50 +0800

    chore: setup dev shell

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NickCao/nixpkgs/nixos-unstable-small";
+  };
+  outputs = { self, nixpkgs, ... }: with nixpkgs.legacyPackages.x86_64-linux; {
+    devShells.x86_64-linux.default = mkShell {
+      nativeBuildInputs = [ cmake ninja llvmPackages_14.clang ];
+      buildInputs = [ llvmPackages_14.llvm ];
+    };
+  };
+}
